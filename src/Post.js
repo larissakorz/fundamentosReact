be2b78bd73bfe4233eb5f1dbe60a4e61c1b72420
Larissa Korz <lariskorz@gmail.com>
2022-09-29 15:48:11 -0300

    renderização condicional

--- src/Post.js
+++ src/Post.js
@@ -4,15 +4,19 @@ import PropTypes from 'prop-types';
 
 // Props -> Propriedades 
 export default function Post(props) {
+   
     return (
     <React.Fragment>
-        <article>
-            <strong>{props.post.title}</strong>
+         <article>
+            <strong>
+                {props.post.read && <s>{props.post.title}</s>}
+                {!props.post.read && props.post.title}
+            </strong>
             <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
             <br />
             <small>{props.post.subtitle}</small>
             <br />
-            Likes: {props.likes / 2}
+            Likes: {props.post.likes / 2}
         </article>
         <br />
     </React.Fragment>
@@ -20,11 +24,12 @@ export default function Post(props) {
 }
 
 Post.propTypes = {
-    likes: PropTypes.number.isRequired,
     onRemove: PropTypes.func.isRequired,
     post: PropTypes.shape({
         id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
         subtitle: PropTypes.string.isRequired,
+        likes: PropTypes.number.isRequired,
+        read: PropTypes.bool.isRequired,
     }).isRequired,
 }
